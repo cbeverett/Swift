48c3c222eaf7b88e1960140dd9d23e1052419c91
Christopher Everett <cbeverett@gatech.edu>
2016-04-17 16:57:16 -0500

    Implemented Exercise 5

---
+++ Exercises/SimpleMath.swift
@@ -0,0 +1,44 @@
+//
+//  SimpleMath.swift
+//  
+//
+//  Created by Christopher Everett on 4/17/16.
+//
+//
+
+import Foundation
+
+func getNumber(name:String) -> Int {
+    var returnValue:Int = -1
+    
+    while returnValue < 0 {
+        print("What is the \(name) number? ", terminator:"")
+        let text = readLine()
+        if let number = NSNumberFormatter().numberFromString(text!) {
+            if number.intValue >= 0 {
+                returnValue = number.integerValue
+            }
+            else {
+                print("Negative numbers not allowed.")
+            }
+        }
+        else {
+            print("\(text!) is not a number.")
+        }
+    }
+    
+    return returnValue
+}
+
+let first: Int = getNumber("first")
+let second: Int = getNumber("second")
+
+var operations : [(String, (Int, Int) -> Int)] = [("+", {$0 + $1}), ("-", {$0 - $1}), ("*", {$0 * $1}), ("/",{$0 / $1}) ]
+
+for (opname, operation) in operations {
+    let result = operation(first,second)
+    print("\(first) \(opname) \(second) = \(result)")
+}
+
+
+
