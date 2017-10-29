73157e60576db5d513b4089741f6574341dcdda7
Christopher Everett <cbeverett@gatech.edu>
2017-10-29 01:50:27 -0500

    Remove old directories

--- CS193P/CalculatorBrain/CalculatorBrain/CalculatorBrain.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CalculatorBrain.swift
-//  CalculatorBrain
-//
-//  Created by Christopher Everett on 3/7/16.
-//  Copyright © 2016 Christopher Everett. All rights reserved.
-//
-
-import Foundation
